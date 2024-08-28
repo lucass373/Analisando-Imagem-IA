cfc0ff438b565d4682b538e7c9c46f05747400c1
lucass373 <78789490+lucass373@users.noreply.github.com>
2024-08-28 10:43:09 -0300

    Revert "remove database"
    This reverts commit 85a5753d27df31ded11ed6223d017a96707b5760.

---
+++ src/models/measureModel.ts
@@ -0,0 +1,18 @@
+interface Measure {
+    measure_uuid: string;
+    measure_datetime: Date;
+    measure_type: 'WATER' | 'GAS';
+    measure_value: string;
+    has_confirmed: boolean;
+    image_url: string;
+  }
+  
+  interface CustomerMeasure {
+    customer_code: string;
+    measures: Measure[];
+  }
+  
+  const measures: CustomerMeasure[] = [];
+  
+  export { measures };
+  
